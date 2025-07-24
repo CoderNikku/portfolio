4ff984bcabec77d1858f440b7d893c81010b3b12
nirmallohiya <nlohiya01@gmail.com>
2025-07-25 01:50:48 +0530

    some adding new logics

--- logic.js
+++ logic.js
@@ -7,3 +7,45 @@
 //     });
 // });
 // });
+
+
+
+const openBtn=document.getElementById('openNav');
+const closeBtn=document.getElementById('closeNav');
+const nav=document.getElementById('nav')
+const navLinks=document.querySelectorAll('.navtag li a');
+
+
+openBtn.addEventListener('click',function(e){
+    e.preventDefault()
+    nav.classList.add('active');
+    openBtn.style.display='none'
+    closeBtn.style.display='block';
+});
+
+
+closeBtn.addEventListener('click',function(e){
+    e.preventDefault()
+    nav.classList.remove('active');
+    openBtn.style.display='block';
+    closeBtn.style.display='none'
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+            nav.classList.remove('active');
+            openBtn.style.display='block';
+            closeBtn.style.display='none';
+        }
+    });
+});
+
+
