d21cda79d715a35d13542ca0f0ec551b760405fc
nirmallohiya <nlohiya01@gmail.com>
2025-07-25 00:01:02 +0530

    add and some chnages

--- logic.js
+++ logic.js
@@ -1,9 +1,9 @@
-document.querySelector('a').forEach(anchor=>{
-    anchor.addEventListener('click',function(e)
-{
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-    });
-});
-});
+// document.querySelector('a').forEach(anchor=>{
+//     anchor.addEventListener('click',function(e)
+// {
+//     e.preventDefault();
+//     document.querySelector(this.getAttribute('href')).scrollIntoView({
+//         behavior: 'smooth'
+//     });
+// });
+// });
